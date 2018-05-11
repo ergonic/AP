c8d9eadd6e896772014a3ef8934fa0ca70030379
Tomáš Kadavý <kadavy4@gmail.com>
2018-05-11 10:04:31 +0200

    minor test

--- AP/testCore.cpp
+++ AP/testCore.cpp
@@ -1,15 +1,31 @@
 #include <stdio.h>
 #include <math.h>
 
+#include <vector>
+
 //windows only
 #include <Windows.h>
 
 //custom inlcude
-#include "GFS.h"
+//#include "GFS.h"
 
+
+typedef struct t_function {
+	char* name;
+	std::vector<Function>(*f)(std::vector<Function>);
+} Function;
+
+std::vector<Function> Sum(std::vector<Function> in) {
+	return { in[0] }/* + in[1]*/;
+}
  
 int main() {
 
+	Function sum;
+	sum.name = "Total";
+	sum.f = &Sum;
+
+	/*
 	GFSALL* gfs =  gfs_init();
 	
 	double ret = 0;
@@ -17,7 +33,7 @@ int main() {
 
 
 	gfs_free(gfs);
-
+	*/
 	system("pause");
 
 	return 0;
